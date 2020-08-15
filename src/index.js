8f50310fd84a18401652a539a941f1ce56e9f970
Yan BUATOIS <yanbuatois@outlook.fr>
2020-08-15 12:27:24 +0200

    Added local storage save.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Components/Game';
 import * as serviceWorker from './serviceWorker';
+import VoltorbFlip from '@dracoctix/voltorb-flip';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const tempLevel = localStorage.getItem('level');
+const startingLevel = (tempLevel && Number.isSafeInteger(+tempLevel) && +tempLevel <= 8 && +tempLevel > 0) ? +tempLevel : 1;
+const tempScore = localStorage.getItem('score');
+const score = (tempScore && Number.isSafeInteger(+tempScore) && +tempScore >= 0) ? +tempScore : 0;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <Game game={new VoltorbFlip({
+      startingLevel,
+      score,
+    })} />
   </React.StrictMode>,
   document.getElementById('root')
 );
